3a8f2d64c388b09c172363174ab52065fdba2fbc
dhimantsoni <dhimantsoni@icloud.com>
2025-08-06 21:32:53 +0530

    Initial Commit

---
+++ RgbWallet/RgbWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  RgbWalletApp.swift
+//  RgbWallet
+//
+//  Created by Dhimant Soni on 06/08/25.
+//
+
+import SwiftUI
+
+@main
+struct RgbWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RgbWalletTests/RgbWalletTests.swift
@@ -0,0 +1,17 @@
+//
+//  RgbWalletTests.swift
+//  RgbWalletTests
+//
+//  Created by Dhimant Soni on 06/08/25.
+//
+
+import Testing
+@testable import RgbWallet
+
+struct RgbWalletTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
